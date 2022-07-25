4aee5b03c6e3b77bd1b84b28cb2bee5b839cfcbc
Daniil Boyko <d.boyko@sxope.com>
2022-07-26 02:43:18 +0300

    feat(factory): refreshing seed of User table, added status column to seed and factory (LF-6)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,8 +21,9 @@ class UserFactory extends Factory
             'name'              => fake()->name(),
             'email'             => fake()->safeEmail(),
             'email_verified_at' => now(),
-            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password'          => Str::random(15), // password
             'remember_token'    => Str::random(10),
+            'status'            => fake()->boolean,
         ];
     }
 
