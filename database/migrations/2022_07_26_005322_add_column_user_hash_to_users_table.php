12ef77f134b248ed3f6e6e3cca09b88eb2c211d4
Daniil Boyko <d.boyko@sxope.com>
2022-07-26 03:55:40 +0300

    feat(migration): added column user_hash to users table (LP-9)

---
+++ database/migrations/2022_07_26_005322_add_column_user_hash_to_users_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        DB::statement('ALTER TABLE users ADD COLUMN user_hash VARCHAR(255)');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        DB::statement('ALTER TABLE users DROP COLUMN user_hash');
+    }
+};
