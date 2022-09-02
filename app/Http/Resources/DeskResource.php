28469a875652dec6ab3c99542fabf2b1fc1bb173
Daniil Boyko <d.boyko@sxope.com>
2022-09-02 17:11:45 +0300

    refactor: added properties to API resources

--- app/Http/Resources/DeskResource.php
+++ app/Http/Resources/DeskResource.php
@@ -5,6 +5,12 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property mixed $created_at
+ * @property integer $id
+ * @property string $name
+ * @property mixed $lists
+ */
 class DeskResource extends JsonResource
 {
     /**
